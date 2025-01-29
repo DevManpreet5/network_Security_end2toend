8feabaf19a808bee3647927aafe337d7363c20a8
Manpreet Singh <singhman2005123@gmail.com>
2025-01-29 18:21:35 +0530

    merged all csv into one df in ingestion pipeline

--- src/component/data_ingestion_comp.py
+++ src/component/data_ingestion_comp.py
@@ -1,6 +1,8 @@
 from src.entity.data_ingestion_entity import DataIngestionConfig
 import os
 import subprocess
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 
 class DataIngestionComponent:
@@ -17,13 +19,37 @@ class DataIngestionComponent:
         subprocess.run(curl_command, shell=True, check=True)
     
     def unzip(self):
+        self.download()
         unzip_command = f"unzip {self.download_path} -d {self.extract_path}"
         subprocess.run(unzip_command, shell=True, check=True)
-        
-
-    def run(self):
-        self.download()
-        self.unzip()
         print("Download and extraction completed successfully!")
+
+    def combine(self):
+        self.unzip()
+        directory_path = self.extract_path
+        csv_files = [file for file in os.listdir(directory_path) if file.endswith(".csv")]
+        df_list = [pd.read_csv(os.path.join(directory_path, file)) for file in csv_files]
+        self.combined_df = pd.concat(df_list, ignore_index=True)
+        self.combined_df.columns = self.combined_df.columns.str.strip()
+        print('df merged successfully!')
+    
+    def savecsv(self):
+        self.combine()
+        os.makedirs(self.config.raw_path, exist_ok=True)
+        csv_path=os.path.join(self.config.raw_path,self.config.file_name)
+        self.combined_df.to_csv(csv_path,index=False)
+        print('csv saved successfully!')
+    
+    
+
+
+        
+        
+    def run(self):
+        # self.savecsv()
+
+
+
+
 
 
