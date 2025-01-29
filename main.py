992b69249b254ee3ae6144d26368549fa25cad43
Manpreet Singh <singhman2005123@gmail.com>
2025-01-29 15:50:02 +0530

    edited main.py to call ingestion pipeline

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+
+if __name__=="__main__":
+    pipelineING=DataIngestionPipeline()
+    pipelineING.run()
--- src/component/data_ingestion_comp.py
+++ src/component/data_ingestion_comp.py
@@ -6,14 +6,14 @@ import subprocess
 class DataIngestionComponent:
     def __init__(self,config:DataIngestionConfig):
         self.config=config
-        self.download_dir = self.config['raw_zip_path']
-        self.extract_path = self.config['raw_csv_path']
+        self.download_dir = self.config.raw_zip_path
+        self.extract_path = self.config.raw_csv_path
         os.makedirs(self.download_dir, exist_ok=True)
         os.makedirs(self.extract_path, exist_ok=True)
-        self.download_path = os.path.join(self.download_dir, self.config['zip_name'])
+        self.download_path = os.path.join(self.download_dir, self.config.zip_name)
     
     def download(self):
-        curl_command = f"curl -L -o {self.download_path} {self.config['dataset_url']}"
+        curl_command = f"curl -L -o {self.download_path} {self.config.dataset_url}"
         subprocess.run(curl_command, shell=True, check=True)
     
     def unzip(self):
