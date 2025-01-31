9b99c2189cb96d9c1a9cc45f287cb96778992e25
Manpreet Singh <singhman2005123@gmail.com>
2025-01-31 20:22:41 +0530

    evaluating pipeline is working

--- dags/model_test.py
+++ dags/model_test.py
@@ -1,6 +1,3 @@
-from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
-from src.pipeline.data_transform_pipeline import DataTransformPipeline
-from src.pipeline.model_training_pipeline import ModelTrainingPipeline
 from src.pipeline.model_evaluate_pipeline import ModelEvaluatingPipeline
 import yaml
 import os
@@ -8,10 +5,6 @@ from airflow import DAG
 from airflow.decorators import task
 from datetime import datetime
 
-def load_config(config_path):
-    with open(config_path, 'r') as file:
-        config = yaml.load(file, Loader=yaml.FullLoader)
-    return config
 
 @task
 def run_pipeline():
