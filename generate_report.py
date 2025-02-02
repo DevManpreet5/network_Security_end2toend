a03fddc04e1d38c31b86726a3542fb67f7eb6345
Manpreet Singh <singhman2005123@gmail.com>
2025-02-02 21:54:15 +0530

    changed generate script to dynamically make new folder based on date

--- generate_report.py
+++ generate_report.py
@@ -7,19 +7,6 @@ from datetime import datetime
 from whylogs.viz import NotebookProfileVisualizer
 from whylogs.viz.drift.column_drift_algorithms import calculate_drift_scores
 
-columns_to_keep = [
-    "Max Packet Length", "Packet Length Variance", "Packet Length Std", "Destination Port",
-    "Avg Bwd Segment Size", "Total Length of Fwd Packets", "Average Packet Size",
-    "Bwd Packet Length Max", "Subflow Fwd Bytes", "Total Length of Bwd Packets",
-    "Fwd Packet Length Max", "Subflow Bwd Bytes", "Bwd Packet Length Std",
-    "Init_Win_bytes_forward", "Packet Length Mean", "Fwd Packet Length Mean",
-    "Bwd Packet Length Mean", "Init_Win_bytes_backward", "Total Fwd Packets",
-    "Avg Fwd Segment Size", "Fwd Header Length.1", "Fwd IAT Max", "Bwd Header Length",
-    "Fwd IAT Std", "Bwd Packets/s", "Fwd Header Length", "Flow Bytes/s", "Idle Mean",
-    "Subflow Bwd Packets", "Total Backward Packets", "act_data_pkt_fwd", "ACK Flag Count",
-    "Subflow Fwd Packets", "Label"
-]
-
 def load_and_sample_data(sample_fraction=0.1):
     reference = pd.read_csv('artifact/data/processed/train.csv')
     current = pd.read_csv('artifact/data/processed/eval.csv')
@@ -31,7 +18,7 @@ def load_and_sample_data(sample_fraction=0.1):
     current.fillna(0, inplace=True)
     return reference, current
 
-def generate_drift_report(reference, current, save_path):
+def generate_drift_report(reference, current, base_save_path):
     reference_profile = whylogs.log(reference)
     current_profile = whylogs.log(current)
     reference_profile_view = reference_profile.view()
@@ -39,16 +26,16 @@ def generate_drift_report(reference, current, save_path):
     
     visualization = NotebookProfileVisualizer()
     visualization.set_profiles(target_profile_view=current_profile_view, reference_profile_view=reference_profile_view)
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    json_filename = f'{timestamp}.json'
-    json_filepath = os.path.join(save_path, json_filename)
     
-    visualization.write(rendered_html=visualization.summary_drift_report(), html_file_name=save_path +f'/{timestamp}')
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    report_folder = os.path.join(base_save_path, timestamp)
+    os.makedirs(report_folder, exist_ok=True)
+    json_filename = os.path.join(report_folder, f"{timestamp}.json")
+    visualization.write(rendered_html=visualization.summary_drift_report(), html_file_name=report_folder +f"/{timestamp}")
     
     scores = calculate_drift_scores(target_view=current_profile_view, reference_view=reference_profile_view, with_thresholds=True)
-    with open(json_filepath, 'w') as json_file:
+    with open(json_filename, 'w') as json_file:
         json.dump(scores, json_file, indent=4, default=str)
-     
 
 def main():
     save_path = os.path.join(os.getcwd(), "reports")
